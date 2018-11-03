4d008c18c1e226f2e90ad6f60bff05fbc1c2ec2f
Krzysztof Banasiak <krzysztofbanasiakk@gmail.com>
2018-11-03 10:05:48 +0100

    commit

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+// wypisz przywitanie w kilku jezykach
+void hej(char* who);
+
 int main()
 {
-    print("hello world\n");
+    hej("krzysztof");
+}
+
+// moje funkcje
+
+void hej(char* who) 
+{
+    printf("hello %s\n", who);
+    printf("witaj %s\n", who);
 }
