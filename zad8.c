d9b8a70ba976bbbccd0ed63db2e054532d5ab30c
kbanasiak <krzysztofbanasiakk@gmail.com>
2018-11-08 10:45:01 +0100

    commit

--- zad8.c
+++ zad8.c
@@ -1,1 +1,15 @@
+//program wczyta 3 liczby rzeczywiste porowna je znajdzie najwieksza najmniejsza poda ich sume oraz iloczyn//
 
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+  float liczba1,liczba2,liczba3;
+  printf("wprowadz 3 liczby calkowite:");
+  scanf("%f %f %f",&liczba1, &liczba2, &liczba3);
+  printf("Suma wynosi: =%f\n"(liczba1+liczba2+liczba3));
+  printf("Iloczyn wynosi =%f\n"(liczba1*liczba2*liczba3));
+  
+}
