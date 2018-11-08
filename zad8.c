3baf7a1a9cc8dcb589e7ffb05c343417b2f1abca
kbanasiak <krzysztofbanasiakk@gmail.com>
2018-11-08 11:13:33 +0100

    commit

--- zad8.c
+++ zad8.c
@@ -9,29 +9,31 @@ int main()
   float liczba1,liczba2,liczba3;
   printf("wprowadz 3 liczby calkowite:");
   scanf("%f %f %f",&liczba1, &liczba2, &liczba3);
-  printf("Suma wynosi: =%f\n"(liczba1+liczba2+liczba3));
-  printf("Iloczyn wynosi =%f\n"(liczba1*liczba2*liczba3));
-  if(liczba1>liczba2&liczba1>liczba3)
+  printf("Suma wynosi: =%f\n",(liczba1+liczba2+liczba3));
+  printf("Iloczyn wynosi =%f\n",(liczba1*liczba2*liczba3));
+  if(liczba1>liczba2&&liczba1>liczba3)
   {
-    printf("Największa liczba to: =%f\n"(liczba1));
+    printf("Największa liczba to: =%f\n",(liczba1));
   }
-  if(liczba2>liczba1&liczba2>liczba3)
+  if(liczba2>liczba1&&liczba2>liczba3)
   {
-    printf("Największa liczba to: =%f\n"(liczba2));
+    printf("Największa liczba to: =%f\n",(liczba2));
   }
-  if(liczba3>liczba1&liczba3>liczba2)
+  if(liczba3>liczba1&&liczba3>liczba2)
   {
-    printf("Największa liczba to: =%f\n"(liczba3));
+    printf("Największa liczba to: =%f\n",(liczba3));
   }
-  if(liczba1<liczba2&liczba1<liczba3)
+  if(liczba1<liczba2&&liczba1<liczba3)
   {
-    printf("Najmniejszaa liczba to: =%f\n"(liczba1));
+    printf("Najmniejszaa liczba to: =%f\n",(liczba1));
   }
-  if(liczba2<liczba1&liczba2<liczba3)
+  if(liczba2<liczba1&&liczba2<liczba3)
   {
-    printf("Najmniejszaa liczba to: =%f\n"(liczba2));
-  if(liczba3<liczba1&liczba3<liczba2)
+    printf("Najmniejszaa liczba to: =%f\n",(liczba2));
+  }
+  if(liczba3<liczba1&&liczba3<liczba2)
   {
-    printf("Najmniejszaa liczba to: =%f\n"(liczba2));
+    printf("Najmniejszaa liczba to: =%f\n",(liczba2));
+  }
 
 }
