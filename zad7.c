16089d9b9541f5711731fc1858e0c2aaf37bbc0c
Krzysztof Banasiak <krzysztofbanasiakk@gmail.com>
2018-11-17 10:07:50 +0100

    commit

--- zad7.c
+++ zad7.c
@@ -11,15 +11,16 @@ int main(){
   if(liczba==0)
   {
     printf("podana liczba to 0 pierwiastek wynosi 0");
+    
     exit(1);
 }
   if(liczba<0)
   {
-    printf("podana liczba jest mniejsza od 0");
+    printf("podana liczba jest mniejsza od 0\n");
+    printf("odwrotnosc =%f",powf(liczba,-1));
     exit(1);
   }
 printf("pierwiastek =%f\n",sqrt(liczba));
 printf("odwrotnosc =%f",powf(liczba,-1));
 
 }
-
