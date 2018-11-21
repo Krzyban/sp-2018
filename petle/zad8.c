ad37af6b2934d37b3c96d26f15c854fb315a4a68
kbanasiak <krzysztofbanasiakk@gmail.com>
2018-11-21 17:47:51 +0100

    commit

--- petle/zad8.c
+++ petle/zad8.c
@@ -1,1 +1,27 @@
+//8. Napisz program rozmieszczający poniższe wzory oddzielnie jeden pod drugim. 
+//Użyj pętli for do generowania wzorów. Do wypisywania gwiazdek należy użyć instrukcji putchar('*') 
+//(powodującej wyświetlanie ich obok siebie).
+//Uwaga: dwa ostatnie wzory wymagają, by każdy wiersz zaczynał się od odpowiedniej liczby spacji.
 
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int i,j,a,b;
+
+
+int main()
+{
+  for(i=1;i<=4;i++)
+  {
+    for(j=1;j<=i;j++)
+      printf("*");
+    printf("\n");
+  }
+  for(a=1;a<=4;a++)
+  {
+    for(b=4;b>=a;b--)
+      printf("*");
+    printf("\n");
+  }
+}
