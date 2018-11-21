942d11b7a11e0311df35cd5c560fd6da538eb07d
kbanasiak <krzysztofbanasiakk@gmail.com>
2018-11-21 12:10:22 +0100

    commit

--- petle/fizzbuzz.c
+++ petle/fizzbuzz.c
@@ -8,21 +8,21 @@ int i;
 
 int main()
 {
-  for(i=1; i<21; i++ ) 
+  for(i=1; i<101; i++ ) 
   {
     if( (i%3)==0 && (i%5)==0 ) {
-        printf("FizzBuzz");
+        printf("FIZZBUZZ\n");
     }
     else if((i%3)==0) 
     {
-        printf("Fizz");
+        printf("Fizz\n");
     }
     else if((i%5)==0) 
     {
-        printf("Buzz");
+        printf("Buzz\n");
     }
     else{
-        printf("%d",i);
+        printf("%d\n",i);
     }
   }
 }
