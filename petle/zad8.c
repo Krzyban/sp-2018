61809ad96cd45706967ae3d90d2b0d72ef496259
kbanasiak <krzysztofbanasiakk@gmail.com>
2018-11-21 18:47:12 +0100

    commit

--- petle/zad8.c
+++ petle/zad8.c
@@ -7,21 +7,86 @@
 #include <math.h>
 #include <stdlib.h>
 
-int i,j,a,b;
+void pierwsza();
+void druga();
+void trzecia();
+void czwarta();
 
 
 int main()
 {
+  pierwsza();
+  druga();
+  trzecia();
+  czwarta();
+}
+
+void pierwsza()
+{
+  int i,j;
   for(i=1;i<=4;i++)
   {
     for(j=1;j<=i;j++)
-      printf("*");
-    printf("\n");
+    {
+      putchar('*');
+    }
+    putchar('\n');
   }
+
+}
+
+void druga()
+{
+  int a,b;
   for(a=1;a<=4;a++)
   {
     for(b=4;b>=a;b--)
-      printf("*");
-    printf("\n");
+    {
+      putchar('*');
+    }
+    putchar('\n');
   }
+
+}
+
+void trzecia()
+{
+  int i,j;
+  for(i=1;i<=4;i++)
+  {
+    for(j=4;j>=1;j--)
+    {
+      if(j>i)
+      {
+      putchar(' ');
+      }
+      else
+      {
+      putchar('*'); 
+      }
+    }
+    putchar('\n');
+  }
+  
+}
+
+void czwarta()
+{
+  int i,j;
+  for(i=4; i>=1;i--)
+  {
+    for(j=4;j>=1;j--)
+    {
+      if(j>i)
+      {
+        putchar(' ');
+      }
+      else
+      {
+        putchar('*');
+      }
+    }
+    putchar('\n');
+  }
+  
 }
