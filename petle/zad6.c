2fb589d4a4c5cb67b39eefcdd36bc4107d99e3a1
kbanasiak <krzysztofbanasiakk@gmail.com>
2018-11-21 11:42:44 +0100

    commit

--- petle/zad6.c
+++ petle/zad6.c
@@ -1,1 +1,31 @@
+//6. Napisz program, który wczyta ze stdin 6 liczb rzeczywistych do tablicy,
+// a następnie wyszuka i wypisze największą i najmniejszą liczbę umieszczoną w tej tablicy.
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
+int i,max,min;
+int main()
+{
+  int tablica[6];
+  printf("podaj 6 liczb do tablicy");
+  for(i=0; i<=5; i++)
+  {
+    scanf("%d",&tablica[i]);
+  }
+  max = 0;
+  for (i = 0; i < 6; ++i)
+  {
+  if (tablica[i] > max)
+    max = tablica[i];
+  }
+  min = 0;
+  for (i = 0; i < 6; ++i)
+  {
+    if (tablica[i] < min)
+    min= tablica[i];
+  }
+  printf("min=%d",min);
+  printf("max=%d",max);
+
+}
