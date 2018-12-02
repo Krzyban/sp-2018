ccb9a6d77e5c79c4eb5a0d8991779ece52af9c84
kbanasiak <krzysztofbanasiakk@gmail.com>
2018-12-02 16:07:07 +0100

    commit

---
+++ petle/elementarz1-4.c
@@ -0,0 +1,19 @@
+// napisac program obliczajace silnie z pewnej niewielkiej liczby naturalnej
+
+#include <stdio.h>
+
+void take_input();
+void calculate_and_print();
+
+int main()
+{
+  int i,n,res;
+  printf("podaj liczbe z ktorej ma byc obliczana silnia");
+  scanf("%d",&n);
+  res=1;
+  for(i=n-1;i>=1;i--)
+  {
+    res=res*i;
+  }
+  printf("silnia wynosi%d",res);
+}
