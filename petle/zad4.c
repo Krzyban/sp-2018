f8262f038d3f197f28d8c1c1026792a52c9ec9f9
kbanasiak <krzysztofbanasiakk@gmail.com>
2018-12-02 10:57:32 +0100

    commit

--- petle/zad4.c
+++ petle/zad4.c
@@ -5,24 +5,34 @@
 #include <math.h>
 #include <stdlib.h>
 
-int i,w,wartosc0;
+void take_input(int *w);
+void count_and_print(int w);
 
 int main()
 {
+  int w;
+  take_input(&w);
+  count_and_print(w);
+}
+
+void take_input(int *w)
+{
   printf("podaj wartosc do ktorej program ma dodawac kolejne liczby");
-  scanf("%d",&w);
-  if(w<1)
+  scanf("%d",w);
+  if(*w < 1)
   {
-    printf("podana liczba to 1 -error");
+    printf("podana liczba to mniej niz 1 -error");
     exit(1);
   }
-  if(w>=1)
+}
+
+void count_and_print(int w)
+{
+  int wartosc0, i;
+  wartosc0=0;
+  for(i=0;i<=w;i++)
   {
-    wartosc0=0;
-    for(i=0;i<=w;i++)
-    {
     wartosc0=wartosc0+i;
-    }
-    printf("suma kolejnych liczb od 0 do n wynosi%d",wartosc0);
   }
+  printf("suma kolejnych liczb od 0 do n wynosi%d",wartosc0);
 }
