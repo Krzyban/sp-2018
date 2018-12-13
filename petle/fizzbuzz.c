668b986f5bcd92c221f16748f4e68f3cd1d9d50a
kbanasiak <krzysztofbanasiakk@gmail.com>
2018-12-13 21:06:24 +0100

    commit

--- petle/fizzbuzz.c
+++ petle/fizzbuzz.c
@@ -4,12 +4,18 @@
 #include <math.h>
 #include <stdlib.h>
 
-int i;
+void FizzBuzz();
 
 int main()
 {
-  for(i=1; i<101; i++ ) 
-  {
+  FizzBuzz();
+}
+
+void FizzBuzz()
+{
+    int i;
+    for(i=1; i<101; i++ ) 
+    {
     if( (i%3)==0 && (i%5)==0 ) 
     {
         printf("FIZZBUZZ\n");
