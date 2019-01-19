c15100ee08d9833c967481d54ffebcae8fa49eb7
Krzysztof Banasiak <krzysztofbanasiakk@gmail.com>
2019-01-19 08:51:57 +0100

    commit

--- advent2018/day2/2_1.c
+++ advent2018/day2/2_1.c
@@ -7,60 +7,60 @@
 
 void czytajplik(char **A, int ilosc, FILE *fp)
 {
-	char slowo[MDN];// wczytanie wyrazów z pliku
-	int i=0;
-	for (i=0;i<ilosc; i++)
-	{
-		fscanf(fp,"%s",slowo);
-		A[i] = (char*) malloc(sizeof(char)*MDN);
-		strcpy(A[i],slowo);//przeniesienie do tablicy A z słowo
-	}
+  char slowo[MDN];// wczytanie wyrazów z pliku
+  int i=0;
+  for (i=0;i<ilosc; i++)
+  {
+    fscanf(fp,"%s",slowo);
+    A[i] = (char*) malloc(sizeof(char)*MDN);
+    strcpy(A[i],slowo);//przeniesienie do tablicy A z słowo
+  }
 }
 
 void zliczlitery(char **A, int a)
 {
-	int x;
-	int dwa = 0;
-	int trzy = 0;
-	int licznik[26]={0};//26x0w tablicy poczatkowych wartości
-	int wynik1=0, wynik2=0;
-	for(int i=0; i<a; i++)
-	{
-		dwa=0;//liczniki 2 i 3
-		trzy=0;
-		int j=0;
-		while(*(A[i]+j) != '\0')
-		{
-			if (*(A[i]+j) >= 'a' && *(A[i]+j) <= 'z')//sprawdza czy jest litera
-			{
-				x = *(A[i]+j) - 'a';//kod asci -a=liczba od 0 do 26 która bedzie miejscem w tablicy
-				licznik[x]++;//dane miejsce zmieni sie z 0 na 1
-			}
-			j++;
-		}
-		for(int c=0; c<26; c++)// kontrola wartosci na pozycjach c w wyrazie
-		{
-			if(licznik[c]==2)
-				dwa=1;
-			if(licznik[c]==3)
-				trzy=1;
-			licznik[c]=0;//zerowanie by nie mieszało sie przy kolejnych wyrazach
-		}
-		if(dwa>0)//nie ma zliczac podwojnie
-			wynik1++;
-		if(trzy>0)
-			wynik2++;
-	}
-	int wynik = wynik1 * wynik2;	
-	printf("Wynik - %d\n", wynik);	
+  int x;
+  int dwa = 0;
+  int trzy = 0;
+  int licznik[26]={0};//26x0w tablicy poczatkowych wartości
+  int wynik1=0, wynik2=0;
+  for(int i=0; i<a; i++)
+  {
+    dwa=0;//liczniki 2 i 3
+    trzy=0;
+    int j=0;
+    while(*(A[i]+j) != '\0')
+    {
+      if (*(A[i]+j) >= 'a' && *(A[i]+j) <= 'z')//sprawdza czy jest litera
+      {
+        x = *(A[i]+j) - 'a';//kod asci -a=liczba od 0 do 26 która bedzie miejscem w tablicy
+        licznik[x]++;//dane miejsce zmieni sie z 0 na 1
+      }
+      j++;
+    }
+    for(int c=0; c<26; c++)// kontrola wartosci na pozycjach c w wyrazie
+    {
+      if(licznik[c]==2)
+        dwa=1;
+      if(licznik[c]==3)
+        trzy=1;
+      licznik[c]=0;//zerowanie by nie mieszało sie przy kolejnych wyrazach
+    }
+    if(dwa>0)//nie ma zliczac podwojnie
+      wynik1++;
+    if(trzy>0)
+      wynik2++;
+  }
+  int wynik = wynik1 * wynik2;	
+  printf("Wynik - %d\n", wynik);	
 }
 
 int main()
 {
-	int a=250;
-	FILE *fp = fopen("slowa","r");
-	char **A = (char**) malloc(a*sizeof(char*));//tablica wyrazów
-	czytajplik(A,a,fp);
-	fclose(fp);
-	zliczlitery(A,a);
+  int a=250;
+  FILE *fp = fopen("slowa","r");
+  char **A = (char**) malloc(a*sizeof(char*));//tablica wyrazów
+  czytajplik(A,a,fp);
+  fclose(fp);
+  zliczlitery(A,a);
 }
--- advent2018/day2/2_2.c
+++ advent2018/day2/2_2.c
@@ -7,53 +7,53 @@
 
 void czytajplik(char **A, int ilosc, FILE *fp)
 {
-	char slowo[MDN];
-	int i=0;
-	for (i=0;i<ilosc; i++)
-	{
-		fscanf(fp,"%s",slowo);
-		A[i] = (char*) malloc(sizeof(char)*MDN);
-		strcpy(A[i],slowo);
-	}
+  char slowo[MDN];
+  int i=0;
+  for (i=0;i<ilosc; i++)
+  {
+    fscanf(fp,"%s",slowo);
+    A[i] = (char*) malloc(sizeof(char)*MDN);
+    strcpy(A[i],slowo);
+  }
 }
 
 void porownaj(char **A, int a)
 {
-	for(int i=1; i<a; i++)
-	{
-		for(int j=0; j<i; j++)//żeby nie porownywalo bez sensu dodatkowo 0z1 i 1z0
-		{
-			int l=0;//zlicza ilośc prawidłowych literek ma byc 25
-			int c=0;//znacznik pozycji w wyrazie 0-25
-			int x=0;//licznik błędów max 1
-			while(x<2 && c<26)//c skacze po wyrazie x sprawdza ilosc błędów
-			{
-				if(*(A[i]+c) == *(A[j]+c))//j jest do porównywania
-				{
-					c++;
-					l++;
-				}
-				else
-				{
-					x++;
-					c++;				
-				}
-			}
-			if(l==25)
-			printf("%s\n%s\n",A[i],A[j]); //wypisz wynik z 2 tablic dla pewności
-		}
-	}
+  for(int i=1; i<a; i++)
+  {
+    for(int j=0; j<i; j++)//żeby nie porownywalo bez sensu dodatkowo 0z1 i 1z0
+    {
+      int l=0;//zlicza ilośc prawidłowych literek ma byc 25
+      int c=0;//znacznik pozycji w wyrazie 0-25
+      int x=0;//licznik błędów max 1
+      while(x<2 && c<26)//c skacze po wyrazie x sprawdza ilosc błędów
+      {
+        if(*(A[i]+c) == *(A[j]+c))//j jest do porównywania
+        {
+          c++;
+          l++;
+        }
+        else
+        {
+          x++;
+          c++;				
+        }
+      }
+      if(l==25)
+      printf("%s\n%s\n",A[i],A[j]); //wypisz wynik z 2 tablic dla pewności
+    }
+  }
 }
 
 int main()
 {
-	int a=250;
-	FILE *fp = fopen("slowa","r");
-	char **A = (char**) malloc(a*sizeof(char*));//zapisuje wyrazy do tablicy
-	czytajplik(A,a,fp);
-	fclose(fp);
-	porownaj(A,a);
-	return 1;
+  int a=250;
+  FILE *fp = fopen("slowa","r");
+  char **A = (char**) malloc(a*sizeof(char*));//zapisuje wyrazy do tablicy
+  czytajplik(A,a,fp);
+  fclose(fp);
+  porownaj(A,a);
+  return 1;
 } 
 
 
