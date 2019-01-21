4c08505c876bc308d138a3ed549293d17570a025
kbanasiak <krzysztofbanasiakk@gmail.com>
2019-01-21 17:06:46 +0100

    commit

--- advent2018/day1/1_1.c
+++ advent2018/day1/1_1.c
@@ -14,45 +14,45 @@ void sum();
 
 int main()
 {
-	FILE *fp = fopen("input","r");//liczy zmienne
-	int a=count(fp);
-	fclose(fp);
-	int tab[a];
-	fopen("input","r");
-	read_data(tab, fp);
-	fclose(fp);
-	sum(tab,a);
-	return 1;
+  FILE *fp = fopen("input","r");//liczy zmienne
+  int a=count(fp);
+  fclose(fp);
+  int tab[a];
+  fopen("input","r");
+  read_data(tab, fp);
+  fclose(fp);
+  sum(tab,a);
+  return 1;
 } 
 void read_data(int *tab, FILE *fp)
 {
-	int a = 0;
-	int x;
-	while(fscanf(fp,"%d",&x) != EOF)
-	{
-		tab[a] = x;
-		a++;
-	}
+  int a = 0;
+  int x;
+  while(fscanf(fp,"%d",&x) != EOF)
+  {
+    tab[a] = x;
+    a++;
+  }
 }
 
 int count(FILE *fp)
 {
-	int a = 0;
-	int x;
-	while(fscanf(fp,"%d",&x) != EOF)
-	{
-		a++;
-	}
-	return a;
+  int a = 0;
+  int x;
+  while(fscanf(fp,"%d",&x) != EOF)
+  {
+    a++;
+  }
+  return a;
 }
 
 void sum(int *tab, int a)
 {
-	int suma = 0;
-	for (int i = 0; i < a; i++)
-	{
-		suma+=tab[i];//suma=suma+tab[1] - sumowanie kolejnych wartosci
-	}
-	printf("Suma  %d\n", suma); // wypisz wynik
+  int suma = 0;
+  for (int i = 0; i < a; i++)
+  {
+    suma+=tab[i];//suma=suma+tab[1] - sumowanie kolejnych wartosci
+  }
+  printf("Suma  %d\n", suma); // wypisz wynik
 } 
 
