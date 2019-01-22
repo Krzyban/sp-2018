533e973af8ab524e16d89462ae6fbc7301dffc6a
kbanasiak <krzysztofbanasiakk@gmail.com>
2019-01-22 11:07:40 +0100

    commit

--- budowanie_programow/zad6.c
+++ budowanie_programow/zad6.c
@@ -8,15 +8,15 @@
 float pierwiastek,liczba;
 int main() 
 {
-    
-    float liczba;
-    printf("program wyliczajacy pierwiastek kwadratowy podaj liczbe do pieriastkowania:\n");
-    scanf("%f",&liczba);
-    if(liczba<0){
-        printf("podana liczba jest ujemna");
-        exit(1);
-    }
-    printf("pierwiastek =%f",sqrt(liczba));
+  float liczba;
+  printf("program wyliczajacy pierwiastek kwadratowy podaj liczbe do pieriastkowania:\n");
+  scanf("%f",&liczba);
+  if(liczba<0)
+  {
+    printf("podana liczba jest ujemna");
+    exit(1);
+  }
+  printf("pierwiastek =%f",sqrt(liczba));
 
 }
- 
+
