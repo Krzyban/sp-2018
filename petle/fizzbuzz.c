1e4de80addba214df91dfd37276e2e7762831a01
kbanasiak <krzysztofbanasiakk@gmail.com>
2019-01-24 13:16:30 +0100

    commit

--- petle/fizzbuzz.c
+++ petle/fizzbuzz.c
@@ -13,23 +13,23 @@ int main()
 
 void FizzBuzz()
 {
-    int i;
-    for(i=1; i<101; i++ ) 
-    {
-    if( (i%3)==0 && (i%5)==0 ) 
-    {
-        printf("FIZZBUZZ\n");
-    }
-    else if((i%3)==0) 
-    {
-        printf("Fizz\n");
-    }
-    else if((i%5)==0) 
-    {
-        printf("Buzz\n");
-    }
-    else{
-        printf("%d\n",i);
+  int i;
+  for(i=1; i<101; i++ ) 
+  {
+  if( (i%3)==0 && (i%5)==0 ) 
+  {
+    printf("FIZZBUZZ\n");
+  }
+  else if((i%3)==0) 
+  {
+    printf("Fizz\n");
+  }
+  else if((i%5)==0) 
+  {
+    printf("Buzz\n");
+  }
+  else{
+    printf("%d\n",i);
     }
   }
 }
